81aa24f43c1289975254150df6bcced52bb2434f
Marce Ramirez <marceram87@gmail.com>
2020-01-15 18:25:07 +0100

    Fetch data from json with axios

--- src/containers/GomesList/GnomesList.js
+++ src/containers/GomesList/GnomesList.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const GnomesList = () => {
-  return (
-    <div>
-      Gnomes List
-    </div>
-  )
+import directoryService from '../../services/directoryService';
+
+class GnomesList extends Component {
+  state = {
+    gnomes: []
+  };
+
+  componentDidMount() {
+    directoryService
+      .getAllGnomes()
+      .then(response => {
+        this.setState({ gnomes: response.data.Brastlewark });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { gnomes } = this.state;    
+    return (
+      <div>
+        <h1>Gnomes List</h1>
+        {gnomes
+          ? gnomes.map(gnome => {
+              console.log(gnome.name);
+            })
+          : null}
+      </div>
+    );
+  }
 }
 
-export default GnomesList
+export default GnomesList;
---
+++ src/services/directoryService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class DirectoryService {
+  constructor() {
+    this.directory = axios.create({
+      baseURL: 'https://raw.githubusercontent.com/rrafols/mobile_test/master'
+    });
+  }
+
+  getAllGnomes() {
+    return this.directory.get('/data.json').then(response => response);
+  }
+
+  // getOneGnome(id) {
+  //   return this.directory.get(`gnomes/${id}`).then(response => response);
+  // }
+}
+
+const directoryService = new DirectoryService();
+
+export default directoryService;
