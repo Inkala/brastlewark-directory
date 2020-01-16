8ecedb587211a786395d78d588b200925620dd79
Marce Ramirez <marceram87@gmail.com>
2020-01-16 12:10:25 +0100

    Add loading spinner

--- src/containers/GomesList/GnomesList.js
+++ src/containers/GomesList/GnomesList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from '../../store/actions/actions';
+import classes from './GnomesList.module.scss';
 
 class GnomesList extends Component {
   componentDidMount() {
@@ -10,16 +11,17 @@ class GnomesList extends Component {
 
   render() {
     const { gnomes } = this.props;
+    const spinner = <div className={classes.loading}><img src="/img/loading.gif" alt="loading" /></div>
     console.log('Gnomes:', gnomes);
     return (
-      <div>
+      <main>
         <h1>Gnomes List</h1>
-        {gnomes.length
+        {(gnomes && gnomes.length)
           ? gnomes.map(gnome => (
               <img key={gnome.id} src={gnome.thumbnail} alt={`${gnome.name} profile`} />
             ))
-          : <p>Loading</p>}
-      </div>
+          : spinner}
+      </main>
     );
   }
 }
