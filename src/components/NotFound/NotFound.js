cf9e9ebbe072be50aaad490b1b27bb10f88f87c5
Marce Ramirez <marceram87@gmail.com>
2020-01-18 17:00:08 +0100

    Create not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './NotFound.module.scss';
 
-const NotFound = () => {
+const NotFound = props => {
   return (
-    <div>
-      Not Found
-    </div>
-  )
-}
+    <section className={classes.notFound}>
+      <div className={classes.notFoundWrapper}>
+        <img src="img/gnome-character.jpg" />
+        <div>
+          <h3>Are you lost, traveler?</h3>
+          <p>Please try anorher route.</p>
+          <Link to="/">Go back to safety</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
