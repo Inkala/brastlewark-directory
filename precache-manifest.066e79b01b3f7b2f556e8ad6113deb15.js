43c097e2102e1b33a89f13bd1e77b44c67065388
Marce Ramirez <marceram87@gmail.com>
2020-01-20 11:30:05 +0100

    Updates

---
+++ precache-manifest.066e79b01b3f7b2f556e8ad6113deb15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486059596ca575a52f08f173109457a5",
+    "url": "/brastlewark-directory/index.html"
+  },
+  {
+    "revision": "9ae683165db4bc55f2d9",
+    "url": "/brastlewark-directory/static/css/main.3c416793.chunk.css"
+  },
+  {
+    "revision": "81236588efba65836b22",
+    "url": "/brastlewark-directory/static/js/2.99152c8c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/brastlewark-directory/static/js/2.99152c8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ae683165db4bc55f2d9",
+    "url": "/brastlewark-directory/static/js/main.c92727ce.chunk.js"
+  },
+  {
+    "revision": "d54bf23267e0cf857950",
+    "url": "/brastlewark-directory/static/js/runtime-main.31369e9e.js"
+  }
+]);
